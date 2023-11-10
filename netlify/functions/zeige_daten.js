8ff91c127882fdbc835f61ddff369f21f6d5762a
Jakob Tietböhl <jati1@uni-bremen.de>
2023-11-10 15:41:47 +0100

    Update zeige_daten.js

--- netlify/functions/zeige_daten.js
+++ netlify/functions/zeige_daten.js
@@ -1,20 +1,40 @@
 // netlify/functions/zeige_daten.js
+const mysql = require('mysql2/promise');
 
 exports.handler = async function (event, context) {
   try {
     const selectedName = event.queryStringParameters.name;
 
-    // Führe hier deine serverseitige Logik aus (z.B., Daten aus einer Datenbank abrufen)
-    const data = {
-      end_name: 'Beispiel-Endname',
-      date: '2023-12-01', // Füge das Datum hier ein
-    };
+    // Verbindung zur MySQL-Datenbank herstellen
+    const connection = await mysql.createConnection({
+      host: 'https://github.com/ToadSmoke/Adventskalendar',
+      user: 'root',
+      password: '',
+      database: 'adventskalendar',
+    });
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify(data),
-    };
+    // Daten aus der Datenbank abrufen
+    const [rows, fields] = await connection.execute('SELECT end_name, date FROM persons WHERE name = ?', [selectedName]);
+
+    // Überprüfen, ob Daten gefunden wurden
+    if (rows.length > 0) {
+      const data = {
+        end_name: rows[0].end_name,
+        date: rows[0].date,
+      };
+
+      return {
+        statusCode: 200,
+        body: JSON.stringify(data),
+      };
+    } else {
+      return {
+        statusCode: 404,
+        body: JSON.stringify({ error: 'Keine Daten gefunden.' }),
+      };
+    }
   } catch (error) {
+    console.error('Fehler:', error);
     return {
       statusCode: 500,
       body: JSON.stringify({ error: 'Internal Server Error' }),
